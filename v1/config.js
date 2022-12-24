b259cbf1eb51e0475eb3e9b621a8866fa0f1d4b0
JulsSkogs <rjsollenskog@gmail.com>
2022-12-24 02:11:23 -0500

    React Rewrite
    I have now hit the point where I wish I had already learned git/GitHub because I feel like this is the perfect use case for a branch, but I don't know. I'll turn my attention to that after the crash course.

---
+++ v1/config.js
@@ -0,0 +1,5 @@
+const config = {
+  url: "https://qaxcjnetpgsuqojzatyr.supabase.co/rest/v1/facts",
+
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFheGNqbmV0cGdzdXFvanphdHlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE1OTEwNjYsImV4cCI6MTk4NzE2NzA2Nn0.m6vPt7X2ZKKQB8tsv-u3rvwvMy74WDtBai3Y3JUsa-s",
+};
